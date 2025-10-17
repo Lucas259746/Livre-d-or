98382d2557f868a8113bb30d14f90d9e0364dff1
Lucas259746 <lucas.rozotte@laplateforme.io>
2025-10-17 09:11:11 +0200

    projet list added

---
+++ views/home/Projet List.php
@@ -0,0 +1,45 @@
+<div class="hero">
+    <div class="hero-content">
+        <h1><?php e($message); ?></h1>
+    </div>
+</div>
+
+<section class="getting-started">
+    <div class="container">
+        <h2>Tous les projet auquel j'ai participé </h2>
+        <div class="steps">
+            <div class="step">
+                <div class="step-number">1</div>
+                <h3>CV</h3>
+                <p><a href="<?php echo url(''); ?>">CV</a></p>
+            </div>
+        </div>
+    </div>
+
+    <section class="getting-started">
+        <div class="container">
+            <h2>Tous les projet auquel j'ai participé </h2>
+            <div class="steps">
+                <div class="step">
+                    <div class="step-number">1</div>
+                    <h3>Fansite</h3>
+                    <p><a href="<?php echo url(''); ?>">Cliqué ici</a></p>
+                </div>
+                <div class="step">
+                    <div class="step-number">2</div>
+                    <h3>Voyage</h3>
+                    <p><a href="<?php echo url(''); ?>">Cliqué ici</a></p>
+                </div>
+                <div class="step">
+                    <div class="step-number">3</div>
+                    <h3>Mediatheque</h3>
+                    <p><a href="<?php echo url(''); ?>">Cliqué ici</a></p>
+                </div>
+            </div>
+        </div>
+    </section>
+    </body>
+    <footer>
+    </footer>
+
+    </html>
